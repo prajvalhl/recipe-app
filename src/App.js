11ea9d6e9ca120157e14c646f3327d82268e1f42
Prajval <prajvalhl@gmail.com>
2023-01-08 14:13:44 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Recipes from "./components/Recipes";
+import Axios from "axios";
 
 function App() {
+  const [search, setSerach] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const APP_ID = "1edcf472";
+  const APP_KEY = "d868d59c172c42effbc54e4b6065c0e6";
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const res = await Axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    setRecipes(res.data.hits);
+  };
+
+  const onInputChange = (e) => {
+    setSerach(e.target.value);
+  };
+
+  const onSearchClick = () => {
+    getRecipes();
+  };
   return (
     <div className="App">
-      <div>
-        <h1>Hello World</h1>
+      <Header
+        search={search}
+        onInputChange={onInputChange}
+        onSearchClick={onSearchClick}
+      />
+      <div className="container">
+        <Recipes recipes={recipes} />
       </div>
     </div>
   );
